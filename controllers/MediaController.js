e85efefe5ca0bc02090012d6b1c785ed76aef171
Nickaykr <krasnovnikita151@gmail.com>
2026-05-15 12:15:23 +0300

    добален конроллер для добавление списков пользователей

--- controllers/MediaController.js
+++ controllers/MediaController.js
@@ -1,6 +1,6 @@
 import { pool } from '../config/database.js';
 
-const getMediaById = async (req, res) => {
+export async function  getMediaById(req, res) {
   try {
     const { id } = req.params;
 
@@ -44,6 +44,7 @@ const getMediaById = async (req, res) => {
     const media = mediaRows[0];
     const actualMediaId = media.media_id; 
     const actualSeasonId = media.season_id;
+  
 
     //Параллельно запрашиваем все связанные данные
     const [
@@ -52,6 +53,7 @@ const getMediaById = async (req, res) => {
       extrasResponse, 
       ratingsResponse,
       userRatingResponse,
+      userListresponse,
       pleerResponse
     ] = await Promise.all([
       pool.query(`
@@ -81,6 +83,13 @@ const getMediaById = async (req, res) => {
           rating 
         FROM ratings 
         WHERE season_id = ? AND user_id = ?`, [actualSeasonId, req.user.user_id]),
+     pool.query(`
+        SELECT 
+          uml.status_id AS user_status_id,
+          ls.name AS status_name
+        FROM user_media_lists uml
+        JOIN list_statuses ls ON uml.status_id = ls.statuses_id 
+        WHERE uml.season_id = ? AND uml.user_id = ?`, [actualSeasonId, req.user.user_id]),
       pool.query(`
         SELECT 
           ms.url, ms.is_active, tt.name as type_name, p.name AS player_name
@@ -96,6 +105,8 @@ const getMediaById = async (req, res) => {
     media.average_rating = ratingsResponse[0][0].average_rating || 0;
     media.total_votes = ratingsResponse[0][0].total_votes || 0;
     media.user_rating = userRatingResponse[0][0]?.rating || 0; 
+    media.user_list_id = userListresponse[0][0]?.user_status_id ; 
+    media.user_list_name = userListresponse[0][0]?.status_name; 
     media.video = pleerResponse[0]; 
 
     res.json(media);
@@ -106,6 +117,37 @@ const getMediaById = async (req, res) => {
   }
 };
 
-export default {
-  getMediaById
-};
+export async function getList (req, res){
+  try {
+    const [rows] = await pool.execute('SELECT statuses_id, name FROM list_statuses');
+    res.json(rows);
+  } catch (error) {
+    res.status(500).json({ message: "Ошибка при получении статусов" });
+  }
+}
+
+export async function postListUser (req, res) {
+  const { season_id, status_id } = req.body;
+  const userId = req.user.user_id; 
+
+  console.log(season_id, status_id, userId)
+  if (!season_id || !status_id) {
+    return res.status(400).json({ message: "Не все поля заполнены" });
+  }
+  
+  try {
+    const query = `
+      INSERT INTO user_media_lists (user_id, season_id, status_id, updated_at)
+      VALUES (?, ?, ?, CURRENT_TIMESTAMP)
+      ON DUPLICATE KEY UPDATE 
+        status_id = VALUES(status_id), 
+        updated_at = CURRENT_TIMESTAMP
+    `;
+
+    await pool.execute(query, [userId, season_id, status_id]);
+    res.json({ success: true, message: "Список успешно обновлен" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Ошибка сервера при сохранении" });
+  }
+}
--- routes/media.js
+++ routes/media.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 const router = Router();
-import mediaController from '../controllers/MediaController.js';
+import {getMediaById, getList, postListUser} from '../controllers/MediaController.js';
 import { getMediaList, getMediaByStatus, getPopularMedia, getMediaByGenre, setRating, searchMedia } from '../controllers/mediaListController.js'; 
 import { auth } from '../middleware/auth.js';
 
@@ -8,8 +8,10 @@ router.get('/filter', getMediaByStatus);
 router.get('/popular', getPopularMedia);
 router.post('/rate', auth, setRating);
 router.get('/search', searchMedia);
+router.get('/statuses', getList);
+router.post('/user-lists', auth, postListUser);
 router.get('/genre/:genreName', getMediaByGenre);
-router.get('/:id', auth, mediaController.getMediaById); 
+router.get('/:id', auth, getMediaById); 
 router.get('/', getMediaList);
 
 export default router;
